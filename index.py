66f4cfcb447a40af82094208c2570521d914b7a2
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2024-06-25 21:58:04 +0200

    small changes

--- index.py
+++ index.py
@@ -14,8 +14,7 @@ def number_guessing_game():
             print("Please enter a valid number.")
             continue
 
-        attempts += 1
-
+        attempts += 1        
         if guess < number_to_guess:
             print("Too low! Try again.")
         elif guess > number_to_guess:
